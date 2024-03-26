550cd0a0a9c34cf1dc81704d30e8706579e5212d
Xcutor <Lekanabass50@gmail.com>
2024-03-26 18:03:03 -0500

    task done

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * binary_tree_node - creates a binary tree node
+ *
+ *@parent: the parent of the node being created
+ *@value: the value to be stored in the node
+ *
+ * Return: a pointer to the new node or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+
+	if (parent == NULL)
+		return (new);
+	else if (parent->left == NULL)
+		parent->left = new;
+	else if (parent->right == NULL)
+		parent->right = new;
+	else
+	{
+		free(new);
+		new = NULL;
+	}
+
+	return (new);
+}
